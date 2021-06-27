c42f0ad723d83e4e0f335a19d1eae02f456b96de
AhmadSailik <asailik1993@gmail.com>
2021-06-27 23:32:56 +0300

    add add post and delet function

--- server.js
+++ server.js
@@ -1,34 +1,36 @@
 'use strict';
 require("dotenv").config(); 
 const express = require("express");
-const app = express();
 const cors = require("cors");
-// const mongoose = require("mongoose");
-app.use(express.json());
-app.use(cors());
-let PORT = process.env.PORT ||3010;
-app.post("/addFood",addFavFood)
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+const PORT = 3010;
+
+server.post("/addFood",addFavFood)
+server.get('/getfood',getfood)
+server.delete('/deleteFood/:index',deltedFood);
 // mongoose.connect("mongodb://localhost:27017", {
 //     useNewUrlParser: true,
 //     useUnifiedTopology: true,
 //   });
 
-  const {
-    sportOutdoorWorksHandler,
-  } = require('./Modules/outDoorWorks');
+//   const {
+//     sportOutdoorWorksHandler,
+//   } = require('./Modules/outDoorWorks');
 
-//http:localhost:3010
-app.get("/", homeHandler);
-function homeHandler(req, res) {
-  res.send("Welcome in Home Route");
-}
-//http:localhost:3010/outdoor_workouts
-app.get("/outdoor_workouts",sportOutdoorWorksHandler);
+// //http:localhost:3010
+// app.get("/", homeHandler);
+// function homeHandler(req, res) {
+//   res.send("Welcome in Home Route");
+// }
+// //http:localhost:3010/outdoor_workouts
+// app.get("/outdoor_workouts",sportOutdoorWorksHandler);
 
 
-app.listen(process.env.PORT || 3010, () => {
-    console.log(`Listening on PORT ${PORT}`);
-  });
+
 
   //////////////
 
@@ -40,7 +42,7 @@ const favFoodSchema = new mongoose.Schema({
   label: String,
   image: String,
   url: String,
-  yield:String,
+  yields:String,
   calories: String,
   mealType :String,
 });
@@ -49,35 +51,38 @@ const UserSchema = new mongoose.Schema({
   foods: [favFoodSchema]
 });
 
-const userFoodModal = mongoose.model('userFood', UserSchema);
 const favFoodModal = mongoose.model('Food', favFoodSchema);
+const userFoodModal = mongoose.model('userFood', UserSchema);
+
 
 function seedingFavFood(){
   const userFavFood = new favFoodModal({
     label: 'hhh',
     image: 'bjb',
     url: 'bkjbkj',
-    yield:'bkb',
+    yields:'bkb',
     calories:'bbkj',
     mealType :'bb'
   })
   userFavFood.save()
 }
 
-// seedingFavFood()
+seedingFavFood()
 
 function seedUser(){
   const userEmail = new userFoodModal({
       email : "asailik1993@gmail.com",
-      foods:[{
+      foods:[
+        {
       label: 'hhh',
       image: 'bjb',
       url: 'bkjbkj',
-      yield:'bkb',
+      yields:'bkb',
       calories:'bbkj',
       mealType :'bb'
     
-       } ]
+       }
+       ]
   
     
     })
@@ -92,13 +97,26 @@ seedUser()
 
 
 
+function getfood (req,res){
+  userFoodModal.find({email:"asailik1993@gmail.com"},function(err,userData){
+    if(err){
+      console.log('error');
+  }
+  else
+  {
+      
+      res.send(userData[0].foods);
+  }
 
+  }) 
+}
 
 // console.log(userFavFood.foods)
 function addFavFood(req,res){
-  const {label,image,url,yield,calories,mealType}=req.body;
-
-  favFoodModal.find({email:email},function(err,userData){
+  const {label,image,url,yields,calories,mealType}=req.body;
+  // consol.log("userData[0]")
+  userFoodModal.find({email:'asailik1993@gmail.com'},function(err,userData){
+   
     if(err){
         console.log('error');
     }
@@ -108,17 +126,39 @@ function addFavFood(req,res){
                label:label,
                image:image,
                url:url,
-               yield:yield,
+               yields:yields,
                calories:calories,
                mealType:mealType,
 
             
            }) 
         userData[0].save();
+        
         res.send(userData[0].foods)
+
     }
 })
 }
+function deltedFood(req,res){
+  const email=req.query;
+  const index=Number(req.params.index);
+  userFoodModal.find({email:"asailik1993@gmail.com"},function(err,userData){
+      if(err){
+          res.send('error')
+      }else{
+          const newData=userData[0].foods.filter((food,idx)=>{
+              if(idx !== index){
+                 return food;
+              }
+          })
+          userData[0].foods=newData;
+          userData[0].save();
+          res.send(userData[0].foods)
+      }
+  })
+}
 
-
+server.listen(3010, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
 
